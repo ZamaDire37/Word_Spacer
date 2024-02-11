b17f2f8c0c2f217030dcdf3ddfadb74f75d03685
ZamaDire <106476895+ZamaDire37@users.noreply.github.com>
2024-02-11 17:11:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words_Spacer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a sentence: ");
+                string inputSentence = Console.ReadLine();
+
+                // Use a method to add spaces between words
+                string resultSentence = AddSpacesBetweenWords(inputSentence);
+
+                Console.WriteLine("Result: " + resultSentence);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+
+        static string AddSpacesBetweenWords(string input)
+        {
+            // Split the input into words
+            string[] words = input.Split(' ');
+
+            // Join the words with spaces in between
+            return string.Join(" ", words);
+        }
+    }
+}
